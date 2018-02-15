410debabe5be4b48ffd240aa2899f986772c3774
Lior-brumberg <34351694+Lior-brumberg@users.noreply.github.com>
2018-02-15 15:05:12 +0200

    LIST command almost done

--- list_check.py
+++ list_check.py
@@ -1,33 +1,46 @@
 # -*- coding: utf-8 -*-
 import os
+import time
 
 
 def dir_files(dir):
-    return os.listdir(dir)
+    currected_files = ''
+    files = os.listdir(dir)
+    for i in files:
+        if '.' in i:
+            i = i.split('.')
+            currected_files += i[0] + '{.' + i[1] + ' file}\n'
+        else:
+            currected_files += i + '\n'
+    return currected_files
 
 
 def file_detail(file):
-    info = os.stat(file)
-    return info
+    return_string = ""
+    info = ['last accessed: ' + time.asctime(time.localtime(os.path.getatime(file))),
+            'last metadata change: ' + time.asctime(time.localtime(os.path.getctime(file))),
+            'last modified: ' + time.asctime(time.localtime(os.path.getmtime(file))),
+            'file size: ' + time.asctime(time.localtime(os.path.getsize(file)))]
+    for i in info:
+        return_string += i + '\n'
+    return return_string
 
 
 def list(args):
-    param = args[0]
-    if os.path.isdir(param):
-        return dir_files(param)
-    if os.path.isfile(param):
-        return file_detail(param)
+    if os.path.isdir(args):
+        return dir_files(args)
+    if os.path.isfile(args):
+        return file_detail(args)
+
 
 commands = {'LIST': list}
+
+
 def main():
-    """
-    Add Documentation here
-    """
     while True:
         c = raw_input('$-->')
         command = c.split()
         print commands[command[0]](command[1])
-
 
 
 if __name__ == '__main__':
