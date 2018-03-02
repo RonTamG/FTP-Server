f43f020fe7a987a15357c5d7789b3f49d8164841
RonTamG <RonTam987@gmail.com>
2018-03-02 22:54:29 +0200

    Changed internal structure to a class(With Server being derived from Commands)

--- test_server.py
+++ test_server.py
@@ -3,7 +3,7 @@
 import socket
 import threading
 
-from commands import KNOWN_COMMANDS
+from commands import Commands
 
 COMMAND = 0
 ARGS = 1
@@ -24,43 +24,63 @@ def get_command_args(request):
     return command, args
 
 
-def main_loop(client):
-    global connections
+class Server(Commands):
+    def __init__(self, my_ip=socket.gethostbyname(socket.gethostname())):
+        super(Server, self).__init__(my_ip)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        p = int(raw_input("port-->"))
+        self.server_socket.bind(("0.0.0.0", p))
+        self.server_socket.listen(1)
+        self.connections = []
 
-    request = client.recv(DATA).replace('\r\n', '')
-    while True:
-        print 'request = ' + request
+    def run(self):
+        while True:
+            client, address = self.server_socket.accept()
+            client.send('220 welcome\r\n')
 
-        if not request:
-            connections.remove(client)
-            client.close()
-            print 'client disconnected'
-            break
+            current_thread = threading.Thread(target=self.main_loop, args=(client,))
+            current_thread.daemon = True
+            current_thread.start()
 
-        command, args = get_command_args(request)
-        try:
-            KNOWN_COMMANDS[command](client, args)
-        except KeyError as e:
-            print e
-            client.send('500 command unknown\r\n')
+            self.connections.append(client)
 
+    def main_loop(self, client):
         request = client.recv(DATA).replace('\r\n', '')
+        while True:
+            print 'request = ' + request
+
+            if not request:
+                self.connections.remove(client)
+                client.close()
+                print 'client disconnected'
+                break
+
+            command, args = get_command_args(request)
+            try:
+                self.command_dict[command](client, args)
+            except KeyError as e:
+                print e
+                client.send('500 command unknown\r\n')
+
+            request = client.recv(DATA).replace('\r\n', '')
 
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    p = int(raw_input("port-->"))
-    s.bind(("0.0.0.0", p))
-    s.listen(1)
-    global connections
-    connections = []
-    while True:
-        client, address = s.accept()
-        client.send('220 welcome\r\n')
-        current_thread = threading.Thread(target=main_loop, args=(client,))
-        current_thread.daemon = True
-        current_thread.start()
-        connections.append(client)
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # p = int(raw_input("port-->"))
+    # s.bind(("0.0.0.0", p))
+    # s.listen(1)
+    # global connections
+    # connections = []
+    # while True:
+    #     client, address = s.accept()
+    #     client.send('220 welcome\r\n')
+    #     current_thread = threading.Thread(target=main_loop, args=(client,))
+    #     current_thread.daemon = True
+    #     current_thread.start()
+    #     connections.append(client)
+    s = Server()
+    s.run()
 
 
 if __name__ == '__main__':
